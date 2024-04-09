550c1b3e130602ebab3942972c0e3869c55cfad9
mylongcode <mylongcode@gmail.com>
2024-04-09 17:04:10 +0300

    add user logic

---
+++ RestaurantService/Logic/User/Interfaces/IUserLogicManager.cs
@@ -0,0 +1,17 @@
+﻿
+using Logic.User.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.User.Interfaces
+{
+    public interface IUserLogicManager
+    {
+        public Task<IEnumerable<UserLogic>> GetAllUsers();
+        public Task<bool> CheckUserPassword(string username, string password);
+        public Task<UserLogic> GetUserById(int id);
+    }
+}
---
+++ RestaurantService/Logic/User/UserLogicManager.cs
@@ -0,0 +1,56 @@
+﻿using Dal.Restaurant.Interfaces;
+using Dal.User.Interfaces;
+using Dal.User.Models;
+using Logic.User.Interfaces;
+using Logic.User.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.User
+{
+    public class UserLogicManager : IUserLogicManager
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserLogicManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> CheckUserPassword(string username, string password)
+        {
+            return await _userRepository.CheckUserPassword(username, password);
+        }
+
+        public async Task<IEnumerable<UserLogic>> GetAllUsers()
+        {
+            return _userRepository.GetAllUsers().Result.Select(u => new UserLogic
+            {
+                Email = u.Email,
+                Password = u.Password,
+                FullName = u.FullName,
+                BirthDate = u.BirthDate,
+                PhoneNumber = u.PhoneNumber,
+                Avatar = u.Avatar
+            });
+        }
+
+        public async Task<UserLogic> GetUserById(int id)
+        {
+            UserDal? user = _userRepository.GetUserById(id).Result;
+            return new UserLogic
+            {
+                Email = user.Email,
+                Password = user.Password,
+                FullName = user.FullName,
+                BirthDate = user.BirthDate,
+                PhoneNumber = user.PhoneNumber,
+                Avatar = user.Avatar
+            };
+        }
+    }
+}
