a29fbd8ca82637254965f636c7b3c7e172c273af
mylongcode <mylongcode@gmail.com>
2024-04-10 16:00:08 +0300

    add account controller

--- RestaurantService/Api/Controllers/AccountController.cs
+++ RestaurantService/Api/Controllers/AccountController.cs
@@ -1,11 +1,15 @@
 ﻿using Api.Models;
 using Dal.EF;
 using Dal.User.Models;
+using Logic.Category.Interfaces;
+using Logic.User.Interfaces;
+using Logic.User.Models;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
 using System.Security.Claims;
@@ -16,12 +20,12 @@ namespace Api.Controllers
 {
     public class AccountController : Controller
     {
-        // тестовые данные вместо использования базы данных
-        private List<Person> people = new List<Person>
+        private readonly IUserLogicManager _userLogicManager;
+
+        public AccountController(IUserLogicManager userLogicManager)
         {
-            new Person {Login="admin@gmail.com", Password="12345", Role = "admin" },
-            new Person { Login="qwerty@gmail.com", Password="55555", Role = "user" }
-        };
+            _userLogicManager = userLogicManager;
+        }
 
         [HttpPost("/token")]
         public IActionResult Token(string username, string password)
@@ -53,13 +57,12 @@ namespace Api.Controllers
 
         private ClaimsIdentity GetIdentity(string username, string password)
         {
-            Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
-            if (person != null)
+            if (_userLogicManager.CheckUserPassword(username, password) != null)
             {
                 var claims = new List<Claim>
                 {
-                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
-                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, username),
+                    //new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
                 };
                 ClaimsIdentity claimsIdentity =
                 new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
@@ -70,11 +73,5 @@ namespace Api.Controllers
             // если пользователя не найдено
             return null;
         }
-    }
-    public class Person
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
     }
 }
